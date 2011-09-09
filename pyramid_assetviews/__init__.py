30dc63b39c4122230f4a0d61bf5ba909db84d3ef
Charlie Choiniere <nek4life@gmail.com>
2011-09-09 08:41:04 -0400

    Added docstrings

--- pyramid_assetviews/__init__.py
+++ pyramid_assetviews/__init__.py
@@ -19,12 +19,35 @@ class SimpleResponse(object):
         return self.app_iter
 
 def asset_view_factory(body, content_type):
+    """ Returns an asset_view function that returns a SimpleResponse object"""
     response = SimpleResponse(body, content_type)
     def asset_view(context, request):
         return response
     return asset_view
 
 def add_asset_views(config, spec, *names):
+    """ A pyramid directive that takes a asset spec and filename keywords
+    and adds them to the pyramid config via the `add_view` function.
+
+    These files added by this directive will be served relative to the Root
+    of the application.  For Example:
+
+    config.add_asset_views("mymodule:static", "favicon.ico", "robots.txt")
+
+    Alternatively you could use argument expansion with longer file lists:
+
+    filenames = ['favicon.ico', 'robots.txt', 'humans.txt', 'crossdomain.xml']
+    config.add_asset_views("mymodule:static", *filenames)
+
+    favicon.ico and robots.txt located in mymodule/static/ will now be served
+    from /favicon.ico and /robots.txt respectively.
+
+    Large files are not meant to be served this way as the current
+    implementation reads the entire file into memory before generating the
+    response object.  The main purpose of this module is to include assets
+    that must be served from the root of the domain such as favicon.ico,
+    robots.txt, and crossdomain.xml.
+    """
     path = abspath_from_asset_spec(spec)
     for name in names:
         content_type = mimetypes.guess_type(name, strict=False)[0]
@@ -35,4 +58,6 @@ def add_asset_views(config, spec, *names):
         config.add_view(asset_view_factory(body, content_type), name=name)
 
 def includeme(config):
+    """ Adds the add_asset_views directive into a pyramid applications config
+    """
     config.add_directive('add_asset_views', add_asset_views)
